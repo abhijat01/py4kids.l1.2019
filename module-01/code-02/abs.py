0f1bd4e935cd02acf9c575d9365af20efd62a3a9
Abhijat Vatsyayan <abhijat.vatsyayan@gmail.com>
2020-05-28 14:31:12 -0400

    Minor fixes

--- module-01/code-02/abs.py
+++ module-01/code-02/abs.py
@@ -6,3 +6,10 @@ for i in range(3):
     for j in range(7):
         print(table[i][j]+" ", end="")
     print()
+
+while True:
+    i = input("I:")
+    j = input("J:")
+    i = int(i)
+    j = int(j)
+    print("--->{}<---".format(table[i][j]))
