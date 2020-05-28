960db570976050137ec871d6862b7e74eeb9b754
Abhijat Vatsyayan <abhijat.vatsyayan@gmail.com>
2020-05-28 14:51:40 -0400

    Added if condition and divisibility checks

---
+++ module-01/code-02/div.py
@@ -0,0 +1,11 @@
+def is_divisible(a, b):
+    answer = False 
+    return answer 
+
+while True: 
+    n1 = input("Enter a:")
+    n1 = int(n1) 
+    n2 = input("Enter b:")
+    n2= int(n2) 
+    check = is_divisible(n1, n2) 
+    print("{} is divisible by {} is : {}".format(n1,n2,check)) 
