ac3d0554c8adf7f54a4c84be289cb2de37ffe4c6
Abhijat Vatsyayan <abhijat.vatsyayan@gmail.com>
2020-06-11 16:36:56 -0400

    Cleaned  up code

--- module-01/lesson04/function_graph.py
+++ module-01/lesson04/function_graph.py
@@ -4,13 +4,6 @@ import turtle as t
 def line(x):
     return 3 * x
 
-def print_function_values(function, x_values):
-    y_values = []
-    for x in x_values:
-        y = function(x)
-        y_values.append(y)
-        print("x={} , y={}".format(x, y))
-
 def plot_function_values(function, x_values):
     first_x = x_values[0]
     first_y = function(first_x)
@@ -24,12 +17,9 @@ def plot_function_values(function, x_values):
 
 
 if __name__ == '__main__':
+    t.shape('turtle')
     xy.make_xy(-500, 500, 50, 'black')
-    x_list = []
-    for i in range(-500, 500, 100):
-        x_list.append(i)
 
-    print_function_values(line, x_list)
     x_list = []
     for i in range(-500, 500, 1):
         x_list.append(i)
